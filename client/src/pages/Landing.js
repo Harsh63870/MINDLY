021c53cf6382ab583b76abc6e3d80ade83b08b00
Harsh63870 <harshvardhanpandey372@gmail.com>
2025-07-02 15:59:50 +0530

    update

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,18 +2,34 @@ import React from "react";
 import Questionnaire from "../components/Questionnaire";
 import { Link } from "react-router-dom";
 import "../styles/Landing.css";
+function Stars() {
+  const stars = Array.from({ length: 100 }).map((_, i) => {
+    const style = {
+      left: `${Math.random() * 100}vw`,
+      top: `${Math.random() * 100}vh`,
+      width: `${Math.random() * 2 + 1}px`,
+      height: `${Math.random() * 2 + 1}px`,
+      animationDuration: `${Math.random() * 2 + 1}s`,
+    };
+    return <div className="star" style={style} key={i} />;
+  });
+  return <div className="stars">{stars}</div>;
+}
 
 function Landing() {
   return (
-    <div className="landing-container">
-      <h1>Welcome to Mindly</h1>
-      <div className="questionnaire">
-        <Questionnaire />
+    <>
+      <Stars />
+      <div className="landing-container">
+        <h1>Welcome to Mindly</h1>
+        <div className="questionnaire">
+          <Questionnaire />
+        </div>
+        <div>
+          <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
+        </div>
       </div>
-      <div>
-        <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
-      </div>
-    </div>
+    </>
   );
 }
 
