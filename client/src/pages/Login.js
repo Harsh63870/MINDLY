96ed128f56d9e38660bccecfb1d911bd32027d9c
Harsh63870 <harshvardhanpandey372@gmail.com>
2025-07-02 16:08:16 +0530

    addedStyletoLogin and Register

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Auth.css";
 
 function Login() {
   return (
-    <div>
+    <div className="auth-container">
       <h2>Login</h2>
-      <form>
-        <input type="email" placeholder="Email" required /><br />
-        <input type="password" placeholder="Password" required /><br />
+      <form className="auth-form">
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
         <button type="submit">Login</button>
       </form>
+      <Link to="/register" className="auth-link">
+        Don't have an account? Register
+      </Link>
     </div>
   );
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Auth.css";
 
 function Register() {
   return (
-    <div>
+    <div className="auth-container">
       <h2>Register</h2>
-      <form>
-        <input type="text" placeholder="Username" required /><br />
-        <input type="email" placeholder="Email" required /><br />
-        <input type="password" placeholder="Password" required /><br />
+      <form className="auth-form">
+        <input type="text" placeholder="Username" required />
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
         <button type="submit">Register</button>
       </form>
+      <Link to="/login" className="auth-link">
+        Already have an account? Login
+      </Link>
     </div>
   );
 }
